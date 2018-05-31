4fc1e1596b4a8e5d5dfc1227e2f61dd8679b67a5
janderson88 <jande081@fiu.edu>
2018-05-31 17:14:55 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,3 @@
+const api_url = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${}&format=json&callback=?`
+const wiki_link = 'https://en.wikipedia.org/wiki'
+const randomEndpoint = '/Special:Random'
